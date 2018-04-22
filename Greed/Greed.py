f5bd1166fbd5a7c8084dfb7411df88c12818354a
Joel <contact@joelrodriguezaleman.net>
2018-04-22 16:37:30 +0200

    GreedKata TDD Iteration: 2 scores can be obtained with the same throw

--- Greed/Greed.py
+++ Greed/Greed.py
@@ -5,9 +5,10 @@ class Greed:
     def score(self, dice):
         if not dice:
             raise NoDiceException()
-        if (dice[0] == 1):
-            return 100
-        elif (dice[0] == 5):
-            return 50
-        else:
-            return 0
+        score = 0
+        for die in dice:
+            if die == 1:
+                score = score + 100
+            if die == 5:
+                score = score + 50
+        return score
--- Greed/GreedTest.py
+++ Greed/GreedTest.py
@@ -17,14 +17,16 @@ class GreedTest(unittest.TestCase):
 
     @data_provider(
         lambda: (
-            (1, 100),
-            (2, 0),
-            (3, 0),
-            (4, 0),
-            (5, 50),
-            (6, 0),
+            ([1], 100),
+            ([2], 0),
+            ([3], 0),
+            ([4], 0),
+            ([5], 50),
+            ([6], 0),
+            ([6], 0),
+            ([1, 5], 150),
         )
     )
-    def testItReturnsTheCorrectScoreDependingOnTheDice(self, die, expected_score):
-        score = self.greed.score(dice=[die])
+    def testItReturnsTheCorrectScoreDependingOnTheDice(self, dice, expected_score):
+        score = self.greed.score(dice=dice)
         self.assertEqual(expected_score, score)
