ba8d2bc4adffc5d406305af8d7da0f97d8a0d101
Joel Rodríguez Alemán <contact@joelrodriguezaleman.net>
2018-04-30 21:34:10 +0200

    GreedKata Add more test cases
    - Add new test case for three-dice combinations.
    - Add test cases for all possible combinations of scores for
      a five-dice throw. In this case I only cover combinations
      of 1s and 5s with scores of three and four of a kind with
      the number 2. At this point, with the other existing tests,
      I'm confident enough to believe that repeating the same
      tests replacing 2 with 3, 4 and 6 is not necessary.

--- Greed/GreedTest.py
+++ Greed/GreedTest.py
@@ -40,6 +40,7 @@ class GreedTest(unittest.TestCase):
 
     @data_provider(
         lambda: (
+            # One-die combinations
             ([1], 100),
             ([2], 0),
             ([3], 0),
@@ -47,10 +48,14 @@ class GreedTest(unittest.TestCase):
             ([5], 50),
             ([6], 0),
             ([6], 0),
+
+            # Two-dice combinations
             ([1, 5], 150),
             ([5, 1], 150),
             ([1, 1], 0),
             ([5, 5], 0),
+
+            # Three-dice combinations
             ([1, 3, 4], 100),
             ([1, 1, 4], 0),
             ([1, 1, 1], 1000),
@@ -61,6 +66,9 @@ class GreedTest(unittest.TestCase):
             ([3, 3, 3], 300),
             ([4, 4, 4], 400),
             ([6, 6, 6], 600),
+            ([1, 5, 6], 150),
+
+            # Four-dice combinations
             ([1, 1, 1, 1], 2000),
             ([2, 2, 2, 2], 400),
             ([3, 3, 3, 3], 600),
@@ -77,12 +85,21 @@ class GreedTest(unittest.TestCase):
             ([5, 3, 3, 3], 350),
             ([5, 4, 4, 4], 450),
             ([5, 6, 6, 6], 650),
+
+            # Five-dice combinations
             ([1, 1, 1, 1, 1], 4000),
             ([2, 2, 2, 2, 2], 800),
             ([3, 3, 3, 3, 3], 1200),
             ([4, 4, 4, 4, 4], 1600),
             ([5, 5, 5, 5, 5], 2000),
             ([6, 6, 6, 6, 6], 2400),
+            ([2, 2, 2, 4, 4], 200),
+            ([2, 2, 2, 1, 4], 300),
+            ([2, 2, 2, 4, 5], 250),
+            ([2, 2, 2, 1, 5], 350),
+            ([2, 3, 2, 1, 5], 150),
+            ([2, 2, 2, 2, 1], 500),
+            ([2, 2, 2, 2, 5], 450),
         )
     )
     def testItReturnsTheCorrectScoreDependingOnTheDice(self, dice, expected_score):
